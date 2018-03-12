0117c8c63d63479ed3a747266cf223c6f11cd56c
cc <1239378293@qq.com>
2018-03-12 13:45:40 +0800

    fix

--- src/main/java/com/wsc/qa/service/CreateCallbackService.java
+++
@@ -1,9 +0,0 @@
-package com.wsc.qa.service;
-
-public interface CreateCallbackService {
-
-//	public String genCallbackStr(String remark);
-
-	String genCallbackStr(String remark, String callbackstatus);
-
-}
